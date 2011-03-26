741d4d4c6bc00c4ef776f33cc27f8cc4aaafb671
Irvin <irvin@mail.moztw.org>
2011-03-26 17:32:02 +0000

    dynamic change frontpage feature background image with detected os by littlebtc
    git-svn-id: http://svn.openfoundry.org/moztw@1162 9cc691a1-34e3-0310-a474-e8860ed5959b

--- js/download.js
+++ js/download.js
@@ -1,28 +1,26 @@
 // piaip's lightweight implementation of download.js
+// March 2011: modified by littlebtc orz
 function getPlatform()
 {
-  if (navigator.platform.indexOf("Win32") != -1)
+  if (navigator.platform.indexOf("Win32") != -1 || navigator.platform.indexOf("Win64") != -1)
     return "win";
   else if (navigator.platform.indexOf("Linux") != -1)
     return "linux";
   else if (navigator.userAgent.indexOf("Mac OS X") != -1)
     return "mac";
-  else if (navigator.platform.indexOf("Mac") != -1)
-    return "mac9";
-  else if (navigator.platform.indexOf("FreeBSD") != -1)
-    return "freebsd";
   return "unknown";
 }
 
 function highlightDownload() {
   var platform = getPlatform();
-  var di = 0;
-  var foundList = document.getElementsByTagName('A');
-  var e = null;
-  for (di = 0; di < foundList.length; di++) {
-    e = foundList[di];
-    if (e.href.indexOf(platform) == -1) continue; //這邊應該會有個 bug 把 mac 跟 mac9 混在一起？
-	e.className = e.className + ' preselected';
+  if (platform != "unknown") { // Unknown 的情況就不 Prefer 任何平台的連結
+    var e = document.getElementById("download-link-" + platform);
+    e.className = e.className + ' preselected';
+  }
+  // 換首頁圖
+  var eFeature = document.getElementById("front-feature");
+  if (eFeature && platform != "unknown" && platform != "win") {
+    eFeature.style.backgroundImage = "url(images/4-feature-" + platform + ".png)";
   }
 }
 
