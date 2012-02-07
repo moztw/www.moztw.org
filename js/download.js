3903d31e4bceaa5353c58a5c44ca95e66483f703
petercpg <petercpg@mail.moztw.org>
2012-02-07 11:00:01 +0000

    Fix for os-dependent images on index page
    git-svn-id: http://svn.openfoundry.org/moztw@1364 9cc691a1-34e3-0310-a474-e8860ed5959b

--- js/download.js
+++ js/download.js
@@ -29,9 +29,9 @@ function highlightDownload() {
   }
   // 換首頁圖
   var eFeature = document.getElementById("front-feature");
-//  if (eFeature && platform != "unknown" && platform != "win") {
-//    eFeature.style.backgroundImage = "url(images/4-feature-" + platform + ".png)";
-//  }
+  if (eFeature && platform != "unknown" && platform != "win") {
+    eFeature.style.backgroundImage = "url(images/4-feature-" + platform + ".png)";
+  }
   
   // 換首頁 Feature Block
   eFeature = document.getElementById("replace-this");
