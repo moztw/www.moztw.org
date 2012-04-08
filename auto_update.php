e9471a005579fad05746e0e81e748cc00da432a4
orinx <orinx@orinx.com>
2012-04-08 07:04:43 +0000

    git-svn-id: http://svn.openfoundry.org/moztw@1410 9cc691a1-34e3-0310-a474-e8860ed5959b

--- auto_update.php
+++ auto_update.php
@@ -6,11 +6,44 @@ include("inc/header.html");
 ?>
 <H1>MozTW Website Online Update</H1>
 <?
+if(isset($_POST["rebuild_git"]) && $_POST["rebuild_git"])
+{
+    # processing
+?>
+Starting Update [GIT]...  
+<pre id='progress_blk'  title='still processing, please wait...'
+    style='padding: 5px; maring: 5px; cursor: wait;
+	    border: 1px solid green; 
+            border-bottom: 1px dotted red'><?
+	$cmd = 'bash /home/moztw/htdocs/autoupdate/update-git.sh';
+    $cmd .= ' 2>&1';
+    echo "Command: " . htmlspecialchars($cmd) . "\n\n";
+
+    ob_flush(); flush(); 
+    // system("" . $cmd); 
+    $handle = popen("" . $cmd, "r"); 
+    while($s= fgets($handle, 1024)) {
+	// read from the pipe
+	print htmlspecialchars($s);
+	fwrite($handle, "p\n");
+	ob_flush(); flush(); 
+    }
+    pclose($handle);
+?>
+</pre><script type='text/javascript'>
+<!--
+document.getElementById('progress_blk').style.border = '1px solid black';
+document.getElementById('progress_blk').style.cursor = 'default';
+document.getElementById('progress_blk').title = 'ready';
+// -->
+</script>
+<?
+}
 if(isset($_POST["rebuild_yes"]) && $_POST["rebuild_yes"])
 {
     # processing
 ?>
-Starting Update...  
+Starting Update [SVN]...  
 <pre id='progress_blk'  title='still processing, please wait...'
     style='padding: 5px; maring: 5px; cursor: wait;
 	    border: 1px solid green; 
@@ -51,11 +84,17 @@ document.getElementById('progress_blk').title = 'ready';
     ?>
     <form method="post"><fieldset>
     <label style='color: blue;'>
-    Basic update:</label><br/><br/>
+    Basic SVN update:</label><br/><br/>
     <input  id='xgeneral' type='checkbox' name='rebuild_yes' checked>
     <label for='xgeneral'>Confirm to update SVN and rebuild.</label><br/>
+	<!-- Modify by orinx start -->
+	<label style='color: blue;'>
+    Basic GIT update:</label><br/><br/>
+	<input  id='xgit' type="checkbox" name="rebuild_git">
+    <label for='xgit'>Confirm to update GIT and rebuild.</label><br/>
+	<!-- Modify by orinx end -->
     <hr><label style='color: blue;'>
-    Optional rebuild process:</label><br/><br/>
+    SVN Optional rebuild process:</label><br/><br/>
     <input  id='xmd5' type="checkbox" name="rebuild_md5"> 
     <label for='xmd5'>Recalculate MD5 information 
 	<b style='color: red;'>(if files in dls/ changed, and very slow)
@@ -73,3 +112,4 @@ document.getElementById('progress_blk').title = 'ready';
 <?
 include("inc/footer.html");
 ?>
+
