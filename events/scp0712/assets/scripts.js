bb0b7d324b9706ead67e36516e6db938d3d3d4ce
Yao Wei <mwei@lxde.org>
2012-08-03 05:39:46 +0000

    added images and remove registration
    git-svn-id: http://svn.openfoundry.org/moztw@1611 9cc691a1-34e3-0310-a474-e8860ed5959b

--- events/scp0712/assets/scripts.js
+++ events/scp0712/assets/scripts.js
@@ -38,3 +38,32 @@ $("body>header a").click(function(){
 $win.resize(function(){
   offset = $nav.offset().top;
 });
+
+$.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?", {
+  ids: "12452841@N00,60061298@N00", // othree, irvinchen
+  tags: "mozparty",
+  format: "json"
+}, function(data){
+  var authorReg = /^nobody@flickr\.com \((.*)\)$/;
+  $.each(data.items, function(i, item){
+    var img = $("<img/>")
+          .attr("src", item.media.m.replace(/_m\./, "_s."))
+          .attr("alt", item.media.title)
+          .data("largeImage", item.media.m.replace(/_m\./, "_b."))
+          .data("author", item.author.match(authorReg)[1])
+          .data("authorLink", "http://www.flickr.com/photos/"+item.author_id)
+          .data("link", item.link);
+
+    $("<li/>")
+          .append(img)
+          .appendTo("#mozGallery .photoList");
+  });
+
+  $("#mozGallery .photoList img").click(function(){
+    $(".singlePhoto img").attr("src", $(this).data("largeImage"));
+    $(".singlePhoto .link").attr("href", $(this).data("link"));
+    $(".singlePhoto .attribution").attr("href", $(this).data("authorLink")).text($(this).data("author"));
+  });
+
+  $("#mozGallery .photoList img:eq(0)").click();
+});
