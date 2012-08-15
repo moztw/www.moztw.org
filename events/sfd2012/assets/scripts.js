3e7d16bd57527d948b341bacea4b3e250ee75c91
thomasysliu <thomasysliu@gmail.com>
2012-08-15 16:23:09 +0000

    Responsive fix
    git-svn-id: http://svn.openfoundry.org/moztw@1637 9cc691a1-34e3-0310-a474-e8860ed5959b

--- events/sfd2012/assets/scripts.js
+++ events/sfd2012/assets/scripts.js
@@ -8,10 +8,73 @@ var cloudmade = new L.TileLayer('http://tile.openstreetmap.tw/tiles/{z}/{x}/{y}.
     zoom: 17
 });
 
-map.addLayer(cloudmade).setView(new L.LatLng(25.043885, 121.530540), 16);
+map.addLayer(cloudmade).setView(new L.LatLng(25.042991, 121.507252), 16);
 
-var marker = new L.Marker(new L.LatLng(25.043887, 121.529045));
+var marker = new L.Marker(new L.LatLng(25.042991, 121.507252));
 map.addLayer(marker);
 
-marker.bindPopup("華山 1914 文創園區").openPopup();
+marker.bindPopup("真善美劇院").openPopup();
 
+
+	// photos
+	var photos = function($photos, time){
+		var photos = $photos.find('li'),
+		len = photos.length - 1,
+		current = 0,
+		timer;
+		
+		function show(index){
+			index = index < 0? 0: (index > len? len: index);
+			photos.eq(index).stop(false, true).fadeIn('fast');
+			photos.eq(current).stop(false, true).fadeOut('fast');
+			current = index;
+		};
+		
+		function next(){
+			show(current + 1 > len? 0: current + 1);
+		};
+		
+		function prev(){
+			show(current - 1 < 0? len: current - 1);
+		};
+		
+		function setTimer(){
+			timer = setTimeout(autoPlay, time);
+		};
+		
+		function clearTimer(){
+			clearTimeout(timer);
+		};
+		
+		function autoPlay(){
+			next();
+			setTimer();
+		};
+		
+		photos.eq(0).css('display', 'block');
+		setTimer();
+		
+		return {
+			ele: $photos,
+			next: next,
+			prev: prev,
+			setTimer: setTimer,
+			clearTimer: clearTimer
+		};
+	}($('#photos .content'), 5000);
+	
+	photos.ele.hover(function(){
+		photos.clearTimer();
+	}, function(){
+		photos.setTimer();
+	});
+	
+	photos.ele.children('.left-btn').on('click', function(){
+		photos.prev();
+		return false;
+	});
+	
+	photos.ele.children('.right-btn').on('click', function(){
+		photos.next();
+		return false;
+	});
