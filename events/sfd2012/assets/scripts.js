db7897efaac53181f0661a1b258198b7de95f937
thomasysliu <thomasysliu@gmail.com>
2012-09-16 08:43:26 +0000

    Add photos and update photo play
    git-svn-id: http://svn.openfoundry.org/moztw@1686 9cc691a1-34e3-0310-a474-e8860ed5959b

--- events/sfd2012/assets/scripts.js
+++ events/sfd2012/assets/scripts.js
@@ -2,79 +2,71 @@ var map = new L.Map('map', {
     scrollWheelZoom: false,
     dragging: false
 });
-
 var cloudmade = new L.TileLayer('http://tile.openstreetmap.tw/tiles/{z}/{x}/{y}.png', {
     attribution: 'Map data <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA 2.0</a> <a href="http://openstreetmap.org">OpenStreetMap</a> contributors.',
     zoom: 17
 });
-
 map.addLayer(cloudmade).setView(new L.LatLng(25.042991, 121.507252), 16);
-
 var marker = new L.Marker(new L.LatLng(25.042991, 121.507252));
 map.addLayer(marker);
-
 marker.bindPopup("真善美劇院").openPopup();
+var userIter = 0;
+// photos
+var photos = function ($photos, time) {
+    $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?", {
+        tags: "moztwcoderush",
+        format: "json"
+    }, function (data) {
+        jQuery.each(data.items, function () {
+            var li = $('<li><a target="_blank" href="' + this.link + '"></a></li>');
+            var img = $('<img src="' + this.media.m.replace(/_m\./, "_b.") + '" width="427" height="640"></a></li>');
+            //console.log(data.items[0].media.m);
+            $('#photos .content ul').append(li);
+            $("a", li).append(img);
+        });
+        photos = $photos.find('li');
+        len = photos.length - 1;
+        //console.log(photos);
+        photos.eq(0).css('display', 'block');
+        setTimer();
+    });
+    var photos = $photos.find('li');
+    len = 0;
+    current = 0;
 
+    function show(index) {
+        index = index < 0 ? 0 : (index > len ? len : index);
+        photos.eq(index).stop(false, true).fadeIn('fast');
+        photos.eq(current).stop(false, true).fadeOut('fast');
+        current = index;
+    };
 
-	// photos
-	var photos = function($photos, time){
-		var photos = $photos.find('li'),
-		len = photos.length - 1,
-		current = 0,
-		timer;
-		
-		function show(index){
-			index = index < 0? 0: (index > len? len: index);
-			photos.eq(index).stop(false, true).fadeIn('fast');
-			photos.eq(current).stop(false, true).fadeOut('fast');
-			current = index;
-		};
-		
-		function next(){
-			show(current + 1 > len? 0: current + 1);
-		};
-		
-		function prev(){
-			show(current - 1 < 0? len: current - 1);
-		};
-		
-		function setTimer(){
-			timer = setTimeout(autoPlay, time);
-		};
-		
-		function clearTimer(){
-			clearTimeout(timer);
-		};
-		
-		function autoPlay(){
-			next();
-			setTimer();
-		};
-		
-		photos.eq(0).css('display', 'block');
-		setTimer();
-		
-		return {
-			ele: $photos,
-			next: next,
-			prev: prev,
-			setTimer: setTimer,
-			clearTimer: clearTimer
-		};
-	}($('#photos .content'), 5000);
-	
-	photos.ele.hover(function(){
-		photos.clearTimer();
-	}, function(){
-		photos.setTimer();
-	});
-	
-	photos.ele.children('.left-btn').on('click', function(){
-		photos.prev();
-		return false;
-	});
-	
-	photos.ele.children('.right-btn').on('click', function(){
-		photos.next();
-		return false;
-	});
+    function next() {
+        show(current + 1 > len ? 0 : current + 1);
+    };
+
+    function prev() {
+        show(current - 1 < 0 ? len : current - 1);
+    };
+
+    function setTimer() {
+        timer = setTimeout(autoPlay, time);
+    };
+
+    function clearTimer() {
+        clearTimeout(timer);
+    };
+
+    function autoPlay() {
+        next();
+        setTimer();
+    };
+    $('.left-btn').on('click', function () {
+        prev();
+        return false;
+    });
+    $('.right-btn').on('click', function () {
+        next();
+        return false;
+    });
+}($('#photos .content'), 5000);
