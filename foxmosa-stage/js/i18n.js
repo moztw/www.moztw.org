ba57754b8c79ce9434707cc94d207d7ad7de7fb0
Poying <poying.me@gmail.com>
2012-09-29 12:57:09 +0000

    foxmosa 跑步！
    git-svn-id: http://svn.openfoundry.org/moztw@1700 9cc691a1-34e3-0310-a474-e8860ed5959b

--- foxmosa-stage/js/i18n.js
+++ foxmosa-stage/js/i18n.js
@@ -1,12 +1,30 @@
-var I18n = function(options){
+'use strict';
+(function(window, $){
+
+/**
+ * events: change, beforeChange, error
+ */
+
+var i18n = function(options, main){
 	var setting = {
 		default: 'zh-TW',
 		baseUrl: 'languages/',
-		languages: [
-			'en-US'
-		]
+		languages: {
+			'zh-TW': '中文 (繁體)',
+			'zh-CN': '中文 (簡體)',
+			'en-US': 'English'
+		}
 	},
-	that;
+	current = window.navigator.userLanguage || window.navigator.language,
+	message,
+	that,
+	cache = {},
+	$e = $('<div>');
+
+	if(typeof options === 'function'){
+		main = options;
+		options = null;
+	}
 
 	if(options){
 		for(var i in setting){
@@ -15,30 +33,63 @@ var I18n = function(options){
 			}
 		}
 	}
+	
+	current = setting.languages.hasOwnProperty(current)? current: setting.default;
 
-	function changeLanguage(lan, fc){
-		loadFile(lan, fc);
+	that = {
+		init: function(){
+			loadFile(current);
+		},
+		getMessage: function(key){
+			return message.hasOwnProperty(key)? message[key]: null;
+		},
+		changeLanguage: function(lan){
+			$e.trigger('beforeChange', current, lan);
+			loadFile(lan);
+		},
+		update: function(selecter, datakey){
+			var ele = $(selecter);
+			ele.each(function(){
+				var $this = $(this),
+				msg;
+
+				if(! datakey){
+					if(! $this.data('i18nkey')){
+						$this.data('i18nkey', $this.text());	
+					}
+					msg = that.getMessage($this.data($this.data('i18nkey')));
+				}else{
+					msg = that.getMessage($this.data(datakey));
+				}
+
+				if(msg){
+					$this.text(msg);
+				}
+			});
+		},
+		on: function(name, fc){
+			$e.bind(name, fc);
+		}
 	};
 
-	function loadFile(language, fc){
+	function loadFile(language){
 		$.get(setting.baseUrl + language + '.json', function(data){
-		
-			that = {
-				get: function(key){
-					return data.hasOwnProperty(key)? data[key]: null;
-				},
-				changeLanguage: changeLanguage
-			};
-			
-			fc(that);
-
+			message = data;
+			current = language;
+			$e.trigger('change', that);
 		}).error(function(){
-			console.log('語言檔載入失敗');
+			$e.trigger('error');
+			//console.log('語言檔載入失敗');
 		});
 	};
-
-	loadFile(setting.default, function(that){
-		
-	});
-
+	
+	return function(){
+		var args = Array.prototype.slice.call(arguments);
+		args.push(that);
+		main.apply(this, args);
+	};
 };
+
+window.i18n = i18n;
+
+})(window, jQuery);
--- foxmosa-stage/js/main.js
+++ foxmosa-stage/js/main.js
@@ -1,6 +1,10 @@
-jQuery(function($){
+jQuery(i18n(function($, that){
+	
+	that.init();
 
-	var i18n = new I18n();
+	that.on('change', function(){
+		that.update('.i18n', 'i18n');
+	});
 
 	$.get('pictures.json', function(data){
 		var picwall = new PicWall($('#foxmosa-gallery'), data),
@@ -13,6 +17,42 @@ jQuery(function($){
 
 		autoChange();
 
-	});	
+	});
 
-});
+	(function(baseurl){
+		var canvas = $("#foxmosa-run")[0],
+		ctx = canvas.getContext("2d"),
+		x = 250, y = 0, speed = 50, index = 0, len = 20,
+		images = [], timer;
+
+		function preload(){
+			var counter = 1;
+			for(var i = 0; i < len; i++){
+				var img = $('<img>').attr('src', baseurl + (i + 1) + '.png').on('load', function(){
+					if(len === counter){
+						complete();
+					}
+					counter++;
+				}).error(function(){
+					alert('圖片預載失敗');
+				});
+
+				images.push(img[0]);
+			}
+		};
+
+		function complete(){
+			timer = setInterval(update, speed);
+		};
+
+		function update(){
+			ctx.clearRect( 0, 0, canvas.width, canvas.height);
+			//console.log(images[index].src);
+			ctx.drawImage(images[index], x, y, 136 * 1.8, 77 * 1.8);
+			index = index === len - 1? 0: index + 1;
+		};
+		
+		preload();
+	})('images/run/');
+
+}));
