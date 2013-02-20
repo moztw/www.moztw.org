b6df101f3b2a78f97d3ffffc8753fcc94dc94df2
thomasysliu <thomasysliu@gmail.com>
2013-02-20 13:57:25 +0800

    Github have both "GitHub Services Web Hook" and "GitHub Hookshot xxxxxx" as user-agent

--- git-received.php
+++ git-received.php
@@ -7,7 +7,7 @@ $isInStage = (strpos($_SERVER["SCRIPT_FILENAME"], 'www-stage') != false);
 // 50.57.231.61/32, 54.235.183.49/32, 54.235.183.23/32, 54.235.118.251/32, 54.235.120.57/32, 54.235.120.61/32, 54.235.120.62/32.
 //$whitelist = array('207.97.227.253', '50.57.128.197','108.171.174.178','50.57.231.61','54.235.183.49','54.235.183.23','54.235.118.251','54.235.120.57','54.235.120.61','54.235.120.62');
 //$isGithub = (in_array($_SERVER['REMOTE_ADDR'], $whitelist) === true);
-$isGithub = isset($_POST["payload"]) && ($_SERVER['HTTP_USER_AGENT'] === "GitHub Services Web Hook");
+$isGithub = isset($_POST["payload"]) && (strpos($_SERVER["HTTP_USER_AGENT"], 'GitHub')  !== false);
 file_put_contents('php://stderr', print_r($_SERVER, TRUE));
 file_put_contents('php://stderr', print_r($isInStage, TRUE));
 file_put_contents('php://stderr', print_r($isGithub, TRUE));
