1fc85339eee07e81376e2c5ca2e0f45a060fc2d2
thomasysliu <thomasysliu@gmail.com>
2013-02-20 13:19:32 +0800

    Add debug info

--- git-received.php
+++ git-received.php
@@ -8,12 +8,24 @@ $isInStage = (strpos($_SERVER["SCRIPT_FILENAME"], 'www-stage') != false);
 //$whitelist = array('207.97.227.253', '50.57.128.197','108.171.174.178','50.57.231.61','54.235.183.49','54.235.183.23','54.235.118.251','54.235.120.57','54.235.120.61','54.235.120.62');
 //$isGithub = (in_array($_SERVER['REMOTE_ADDR'], $whitelist) === true);
 $isGithub = isset($_POST["payload"]) && ($_SERVER['HTTP_USER_AGENT'] === "GitHub Services Web Hook");
+file_put_contents('php://stderr', print_r($_SERVER, TRUE));
+file_put_contents('php://stderr', print_r($isInStage, TRUE));
+file_put_contents('php://stderr', print_r($isGithub, TRUE));
+file_put_contents('php://stderr', print_r($_POST, TRUE));
 if( $isInStage && $isGithub ){
   $cmd = 'bash /home/moztw/htdocs/autoupdate/update-git.sh';
   $cmd .= ' 2>&1';
+  file_put_contents('php://stderr', "Command: " . $cmd . "\n\n");
   ob_flush();
   flush();
   $handle = popen("" . $cmd, "r");
+  while($s= fgets($handle, 1024)) {
+    // read from the pipe
+    file_put_contents('php://stderr', $s );
+    fwrite($handle, "p\n");
+    ob_flush();
+    flush();
+  }
   pclose($handle);
 }
 ?>
