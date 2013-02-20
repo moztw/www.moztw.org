82148c993094bec7e5681ed59ae1f798b3ed8504
thomasysliu <thomasysliu@gmail.com>
2013-02-20 12:55:43 +0800

    Use user-agent and payload check

--- git-received.php
+++ git-received.php
@@ -5,11 +5,10 @@ $isInStage = (strpos($_SERVER["SCRIPT_FILENAME"], 'www-stage') != false);
 //
 // The Public IP addresses for these hooks are: 207.97.227.253/32, 50.57.128.197/32, 108.171.174.178/32, 
 // 50.57.231.61/32, 54.235.183.49/32, 54.235.183.23/32, 54.235.118.251/32, 54.235.120.57/32, 54.235.120.61/32, 54.235.120.62/32.
-$whitelist = array('207.97.227.253', '50.57.128.197','108.171.174.178','50.57.231.61','54.235.183.49','54.235.183.23','54.235.118.251','54.235.120.57','54.235.120.61','54.235.120.62');
-$isGithub = (in_array($_SERVER['REMOTE_ADDR'], $whitelist) === true);
-var_dump($isInStage);
-var_dump($isGithub);
-if( $isInStage && $isGithub){
+//$whitelist = array('207.97.227.253', '50.57.128.197','108.171.174.178','50.57.231.61','54.235.183.49','54.235.183.23','54.235.118.251','54.235.120.57','54.235.120.61','54.235.120.62');
+//$isGithub = (in_array($_SERVER['REMOTE_ADDR'], $whitelist) === true);
+$isGithub = isset($_POST["payload"]) && ($_SERVER['HTTP_USER_AGENT'] === "GitHub Services Web Hook");
+if( $isInStage && $isGithub ){
   $cmd = 'bash /home/moztw/htdocs/autoupdate/update-git.sh';
   $cmd .= ' 2>&1';
   ob_flush();
