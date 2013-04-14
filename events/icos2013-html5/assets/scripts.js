83bdf9c72d0ad09c290ac20b991234a481c806d4
小喵 (Ming Tsay) <mt@aa.am>
2013-04-15 00:27:16 +0800

    update map

--- events/icos2013-html5/assets/scripts.js
+++ events/icos2013-html5/assets/scripts.js
@@ -6,8 +6,8 @@ var cloudmade = new L.TileLayer('http://tile.openstreetmap.tw/tiles/{z}/{x}/{y}.
     attribution: 'Map data <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA 2.0</a> <a href="http://openstreetmap.org">OpenStreetMap</a> contributors.',
     zoom: 17
 });
-map.addLayer(cloudmade).setView(new L.LatLng(25.035411, 121.543933), 16);
-var marker = new L.Marker(new L.LatLng(25.035411, 121.543933));
+map.addLayer(cloudmade).setView(new L.LatLng(24.936977, 121.376931), 16);
+var marker = new L.Marker(new L.LatLng(24.936977, 121.376931));
 map.addLayer(marker);
-marker.bindPopup("慶隆商務俱樂部").openPopup();
+marker.bindPopup("國家教育研究院三峽院區").openPopup();
 var userIter = 0;
