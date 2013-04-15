b8abf70d8be71260ea7021af10e86c20cecb7ab8
小喵 (Ming Tsay) <mt@aa.am>
2013-04-15 22:39:35 +0800

    mouse event version 2 bug fix 4

--- events/icos2013-html5/assets/scripts.js
+++ events/icos2013-html5/assets/scripts.js
@@ -14,15 +14,15 @@ var userIter = 0;
 
 (function($){
     $("#schedule").addClass("jq");
-    $("#schedule .caption").click(function(){
+    $(".caption").click(function(){
         $(this).removeClass("caption").addClass("captionClicked")
         .parent("td").parent("tr").after("<tr class=\".introCreated\"><td colspan=\"3\">" + $(this).next(".intro:nth-of-type(1)").html() + "</td></tr>");
     });
-    $("#schedule .captionClicked").click(function(){
+    $(".captionClicked").click(function(){
         $(this).removeClass("captionClicked").addClass("caption")
         .parent("td").parent("tr").next(".introCreated").remove();
     });
-    $("#schedule tr.introCreated td").click(function(){
+    $("tr.introCreated td").click(function(){
         $(this).parent("tr").prev()
         .children("td").children(".captionClicked")
         .removeClass("captionClicked").addClass("caption");
