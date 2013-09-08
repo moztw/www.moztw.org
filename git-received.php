956f64d3ef6f07d1007674db5060bdb3322ab28c
Peter Pin-Guang Chen <petercpg@mail.moztw.org>
2013-09-09 02:20:42 +0800

    Update automatic deploy script to support new repository design

--- git-received.php
+++ git-received.php
@@ -3,24 +3,25 @@ $isInStage = (strpos($_SERVER["SCRIPT_FILENAME"], 'www-stage') != false);
 // Github We’ll hit these URLs with POST requests when you push to us, 
 // passing along information about the push. More information can be found in the Post-Receive Guide.
 //
-// The Public IP addresses for these hooks are: 207.97.227.253/32, 50.57.128.197/32, 108.171.174.178/32, 
-// 50.57.231.61/32, 54.235.183.49/32, 54.235.183.23/32, 54.235.118.251/32, 54.235.120.57/32, 54.235.120.61/32, 54.235.120.62/32.
-//$whitelist = array('207.97.227.253', '50.57.128.197','108.171.174.178','50.57.231.61','54.235.183.49','54.235.183.23','54.235.118.251','54.235.120.57','54.235.120.61','54.235.120.62');
+// The Public IP addresses for these hooks are: 204.232.175.64/27, 192.30.252.0/22. 
+// FIXME: counting CIDR is too complicated. DO NOT UNMARK FOLLOWING TWO LINES UNLESS YOU WROTE CODE FOR COUNTING IP RANGE.
+//$whitelist = array('204.232.175.64/27', '192.30.252.0/22');
 //$isGithub = (in_array($_SERVER['REMOTE_ADDR'], $whitelist) === true);
 $isGithub = isset($_POST["payload"]) && (strpos($_SERVER["HTTP_USER_AGENT"], 'GitHub')  !== false);
-if( $isInStage && $isGithub ){
+if($isGithub){
   $payload = json_decode($_POST["payload"]);
   
-  $cmd = 'bash /home/moztw/htdocs/autoupdate/update.sh';
+  $cmd = '/home/moztw/repo/base/autoupdate/update.sh';
   
   $opt = '';
   if($payload->ref==="refs/heads/production"){
 	//production
-  }else if($payload->ref==="refs/heads/master"){
+	$opt .= ' www';
+  }elseif($payload->ref==="refs/heads/master"){
 	//stage
 	$opt .= ' stage';  
   }else{
-	return;  
+	die("!!!Wrong payload.");
   }
   
   $opt .= ' md5';
@@ -34,5 +35,7 @@ if( $isInStage && $isGithub ){
   flush();
   $handle = popen("" . $cmd, "r");
   pclose($handle);
+} else {
+  die("!!!Invalid arguments given or not from Github.");
 }
 ?>
