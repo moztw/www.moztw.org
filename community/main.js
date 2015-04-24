dbb6f90b75c242ff7ded5621316a96b941186d90
Peter Pin-Guang Chen <petercpg@mail.moztw.org>
2015-04-24 13:37:10 +0800

    https friendly; update and fix links

--- community/main.js
+++ community/main.js
@@ -3,18 +3,17 @@
 
 (function (window) {
 	'use strict';
-	
+
 	var body,
 	    newsRenderer,
 	    rssScript;
-	
+
 	body = window.document.getElementsByTagName('body');
 	body = body[0];
 
 	rssScript = window.document.createElement('script');
-  //rssScript.src = "http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=http://groups.google.com/group/moztw-general/feed/atom_v1_0_msgs.xml&callback=newsRenderer&scoring=h&num=20";
-  rssScript.src = "http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=https://groups.google.com/forum/feed/moztw-general/topics/rss.xml&callback=newsRenderer&scoring=h&num=20";
-	
+  rssScript.src = "https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=https://groups.google.com/forum/feed/moztw-general/topics/rss.xml&callback=newsRenderer&scoring=h&num=20";
+
 	body.appendChild(rssScript);
 
 	newsRenderer = function (data) {
@@ -32,10 +31,10 @@
 		    i,
 		    btn,
 		    form;
-		
+
 		data.forEach(function (val) {
 			val.title = val.title.replace(/(Re|回覆)(:|：) ?/g, '').replace(/\[(moztw-general|MozTW-Dev)\] ?/g, '');
-			
+
 			if (!thread.hasOwnProperty(val.title)) {
 				thread[val.title] = current = {
 					authors: [],
@@ -49,7 +48,7 @@
 
 			current.authors.push(val.author.replace(/\@([\w.]+)/, ''));
 		});
-		
+
 		for (i in thread) {
 			if (thread.hasOwnProperty(i)) {
 				if (counter >= 10) {
@@ -62,7 +61,7 @@
 				tmp += '<p>' + (current.date.getMonth() + 1) + '/' + current.date.getDate() + ' ' + current.date.getHours() + ':' + current.date.getMinutes() + '<br />';
 				tmp += '<a href="' + current.link + '">' + i + '</a><br />';
 				tmp += current.authors.slice(0, 3).join(', ');
-				
+
 				if (current.authors.length > 3) {
 					tmp += ' 等' + (current.authors.length - 3) + '篇';
 				}
