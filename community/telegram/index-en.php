1e86a85fea58ff46191b10b7624f70da5c8b060c
Sean <sean@sean.taipei>
2016-04-05 05:14:30 -0400

    Fix Telegram Join Page
    Fix Default Language

--- community/telegram/index-en.php
+++ community/telegram/index-en.php
@@ -2,7 +2,7 @@
 require('config.php');
 
 if (!isset($_COOKIE['tg_lang'])) {
-	if (preg_match('/zh/', $SERVER['HTTP_ACCEPT_LANGUAGE'])) {
+	if (preg_match('/zh/', $_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
 		setcookie('tg_lang', 'zh', time() + 365*24*60*60, '/community/telegram');
 	} else {
 		setcookie('tg_lang', 'en', time() + 365*24*60*60, '/community/telegram');
