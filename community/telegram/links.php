1b9f9cf85c5884ee0a33873c89e66c7ace4eb6d7
hanbz <hanbzsky@yahoo.com.tw>
2016-08-21 15:03:51 +0800

    issue447-add the introduction to channel (#451)

--- community/telegram/links.php
+++ community/telegram/links.php
@@ -1,7 +1,6 @@
 <?php
 
 $tags = Array(
-	'all' => 'All',
 	'en' => 'English',
 	'zh-tw' => 'Chinese (Taiwan)',
 );
@@ -11,9 +10,19 @@ $links = Array(
 		'id' => 'AmWeuT0XZKuwaB9m3AXfyw',
 		'name' => 'MozTW',
 		'description' => 'MozTW 社群綜合討論群組',
+		'introduction' => '通用頻道。歡迎新手從此開始參與，各式各樣的專案、想法都可以在此提出。',
 		'tags' => Array(
-			'all',
 			'zh-tw',
+		),
+	),
+
+	Array(
+		'id' => 'AmWeuT0XZKuwaB9m3AXfyw',
+		'name' => 'MozTW',
+		'description' => 'General channel. Mainly uses Chinese, but most people understands English.',
+		'introduction' => 'General channel. Mainly uses Chinese, but most people understands English.',
+		'tags' => Array(
+			'en',
 		),
 	),
 
@@ -21,8 +30,8 @@ $links = Array(
 		'id' => 'AutpyAC4ZZEyZXJt9fuRyw',
 		'name' => 'MozTW 字幕組',
 		'description' => 'Mozilla 相關影片中英文字幕製作',
+		'introduction' => '影片字幕子頻道。關於 Mozilla 相關的影片字幕的翻譯與時間軸討論。',
 		'tags' => Array(
-			'all',
 			'zh-tw',
 		),
 	),
@@ -31,8 +40,8 @@ $links = Array(
 		'id' => 'AutpyAHfYs0pkzmAhUYHdw',
 		'name' => 'MozTW Design',
 		'description' => 'Foxmosa 插畫、社群設計討論',
+		'introduction' => '設計子頻道。關於 Mozilla 相關的美工、圖案的設計討論。',
 		'tags' => Array(
-			'all',
 			'zh-tw',
 		),
 	),
@@ -41,8 +50,8 @@ $links = Array(
 		'id' => 'AfQlCwDFQfpL6bDCEJuv8g',
 		'name' => 'MozTW 手工藝群組',
 		'description' => 'MozTW 手工藝設計討論',
+		'introduction' => '手工藝子頻道。例如拼豆、布偶等，主要以與 Mozilla 相關周邊為主，好玩吸引人，軟性的設計討論。',
 		'tags' => Array(
-			'all',
 			'zh-tw',
 		),
 	),
@@ -51,8 +60,8 @@ $links = Array(
 		'id' => 'AutpyAJlAH9QUsil-Rut1w',
 		'name' => 'Foxfooders (zh)',
 		'description' => 'B2G OS 中文討論群',
+		'introduction' => 'Foxfooder 專案台灣子頻道（專案已停止）。另有 B2G 頻道。',
 		'tags' => Array(
-			'all',
 			'zh-tw',
 		),
 	),
@@ -61,28 +70,38 @@ $links = Array(
 		'id' => 'AutpyAabEGUV5IXK4bReIw',
 		'name' => 'MozTW dev/add-on',
 		'description' => 'Mozilla 套件與開發相關技術討論',
+		'introduction' => '開發者及套件子頻道。',
 		'tags' => Array(
-			'all',
 			'zh-tw',
 		),
 	),
-  
-  Array(
+
+	Array(
 		'id' => '@moztwkaohsiung',
 		'name' => 'MozTW@高雄',
 		'description' => 'MozTW 社群在高雄的一個分支',
+		'introduction' => '高雄摩茲事務子頻道。',
 		'tags' => Array(
-			'all',
 			'zh-tw',
 		),
 	),
-  
+
 	Array(
 		'id' => '@mozillian',
 		'name' => 'Mozillians',
 		'description' => 'Mozills general discussion group',
+		'introduction' => '國際摩茲人子頻道，以英文為主要溝通語言。',
 		'tags' => Array(
-			'all',
+			'zh-tw',
+		),
+	),
+
+	Array(
+		'id' => '@mozillian',
+		'name' => 'Mozillians',
+		'description' => 'Mozills general discussion group',
+		'introduction' => 'Sub channel for international Mozillians.',
+		'tags' => Array(
 			'en',
 		),
 	),
@@ -91,8 +110,18 @@ $links = Array(
 		'id' => '@mozvideol10n',
 		'name' => 'mozilla-videol10n',
 		'description' => 'Mozilla\'s video subtitle group',
+		'introduction' => '摩茲影片國際在地化頻道，以英文為主要溝通語言。',
 		'tags' => Array(
-			'all',
+			'zh-tw',
+		),
+	),
+
+	Array(
+		'id' => '@mozvideol10n',
+		'name' => 'mozilla-videol10n',
+		'description' => 'Mozilla\'s video subtitle group',
+		'introduction' => 'Sub channel for localization of Mozilla related videos.',
+		'tags' => Array(
 			'en',
 		),
 	),
