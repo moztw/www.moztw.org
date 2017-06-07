195bf63ed3c9a332801a7e3f1af4e3d4448613b3
Irvin <irvinfly@gmail.com>
2017-06-08 01:10:15 +0800

    /tg: 調整順序

--- community/telegram/links.php
+++ community/telegram/links.php
@@ -51,7 +51,7 @@ $links = Array(
 		'id' => 'AmWeuT0XZKu55YVMu6QEGw',
 		'name' => 'MozTW',
 		'description' => 'Mozilla Taiwan Community channel',
-		'introduction' => 'Mozilla Taiwan Community\'s channel. Mainly uses Chinese, most people also understands English.',
+		'introduction' => 'Mozilla Taiwan Community\'s channel. Chinese mainly, most people also known English.',
 		'tags' => Array(
 			'en',
 		),
@@ -61,7 +61,7 @@ $links = Array(
 		'id' => 'AAAAAD7njcPEZGzr84AIZw',
 		'name' => 'Mozilla HK',
 		'description' => 'Mozilla Hong Kong',
-		'introduction' => '香港社群討論頻道。Mozilla Hong Kong community channel.',
+		'introduction' => 'Mozilla Hong Kong community channel.',
 		'tags' => Array(
 			'zh-tw',
       'en'
@@ -94,61 +94,11 @@ $links = Array(
 		'description' => 'Mozilla VR 社群頻道',
 		'introduction' => 'MozillaVR 貢獻者討論頻道，包含 A-Frame & WebVR。以英文為主。',
 		'tags' => Array(
-			'zh',
-		),
-	),
-
-	Array(
-		'id' => 'AutpyAC4ZZEyZXJt9fuRyw',
-		'name' => 'MozTW 字幕組',
-		'description' => 'Mozilla 相關影片中英文字幕製作',
-		'introduction' => '影片字幕子頻道。關於 Mozilla 相關的影片字幕的翻譯與時間軸討論。',
-		'tags' => Array(
 			'zh-tw',
 		),
 	),
 
-	Array(
-		'id' => 'AutpyAHfYs0pkzmAhUYHdw',
-		'name' => 'MozTW Design',
-		'description' => 'Foxmosa 插畫、社群設計討論',
-		'introduction' => '設計子頻道。關於 Mozilla 相關的美工、繪圖等設計相關討論。',
-		'tags' => Array(
-			'zh-tw',
-		),
-	),
-
-	Array(
-		'id' => 'AfQlCwDFQfpL6bDCEJuv8g',
-		'name' => 'MozTW 手工藝群組',
-		'description' => 'MozTW 手工藝設計討論',
-		'introduction' => '手工藝子頻道。例如拼豆、布偶等，主要以與 Mozilla 相關周邊為主，好玩吸引人，軟性的設計討論。',
-		'tags' => Array(
-			'zh-tw',
-		),
-	),
-
-	Array(
-		'id' => 'AutpyAJlAH9QUsil-Rut1w',
-		'name' => 'Foxfooders (zh)',
-		'description' => 'B2G OS 中文討論群',
-		'introduction' => 'Foxfooder 專案台灣子頻道，B2G OS 中文討論群（專案已中止）。',
-		'tags' => Array(
-			'zh-tw',
-		),
-	),
-
-	Array(
-		'id' => '@moztwkaohsiung',
-		'name' => 'MozTW@高雄',
-		'description' => 'MozTW 社群在高雄的一個分支',
-		'introduction' => '高雄摩茲事務子頻道。',
-		'tags' => Array(
-			'zh-tw',
-		),
-	),
-
-	Array(
+  Array(
 		'id' => '@rust_tw',
 		'name' => 'RustTW',
 		'description' => 'RustTW 社群',
@@ -159,10 +109,60 @@ $links = Array(
 	),
 
 	Array(
+		'id' => 'AutpyAC4ZZEyZXJt9fuRyw',
+		'name' => 'MozTW 字幕組',
+		'description' => 'Mozilla 相關影片中英文字幕製作',
+		'introduction' => '影片字幕子頻道。Mozilla 相關影片的字幕翻譯製作。',
+		'tags' => Array(
+			'zh-tw',
+		),
+	),
+
+	Array(
 		'id' => '@mozvideol10n',
 		'name' => 'mozilla-videol10n (en)',
 		'description' => 'Mozilla\'s video subtitle group',
 		'introduction' => 'Mozilla 相關影片在地化頻道，以英文為主要溝通語言。',
+		'tags' => Array(
+			'zh-tw',
+		),
+	),
+
+	Array(
+		'id' => 'AutpyAHfYs0pkzmAhUYHdw',
+		'name' => 'MozTW Design',
+		'description' => 'Foxmosa 插畫、社群設計討論',
+		'introduction' => '設計子頻道。以 Mozilla 為主題的設計、繪圖等相關討論。',
+		'tags' => Array(
+			'zh-tw',
+		),
+	),
+
+	Array(
+		'id' => 'AfQlCwDFQfpL6bDCEJuv8g',
+		'name' => 'MozTW 手工藝群組',
+		'description' => 'MozTW 手工藝設計討論',
+		'introduction' => '手工藝子頻道。例如拼豆、布偶、烘培⋯⋯等，與 Mozilla 相關為主，好玩吸引人，軟性的設計討論。',
+		'tags' => Array(
+			'zh-tw',
+		),
+	),
+
+	Array(
+		'id' => 'AutpyAJlAH9QUsil-Rut1w',
+		'name' => 'Foxfooders (zh)',
+		'description' => 'B2G OS 中文討論群',
+		'introduction' => 'Foxfooder 專案子頻道，B2G OS 中文討論群（專案已停止）。',
+		'tags' => Array(
+			'zh-tw',
+		),
+	),
+
+	Array(
+		'id' => '@moztwkaohsiung',
+		'name' => 'MozTW@高雄',
+		'description' => 'MozTW 社群在高雄的一個分支',
+		'introduction' => '高雄摩茲事務子頻道。',
 		'tags' => Array(
 			'zh-tw',
 		),
