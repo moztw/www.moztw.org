78c0fc032511986b6b71ee2211ff9d09fea0439e
Ming Tsay <mt@moztw.org>
2017-11-13 16:33:45 +0800

    reformat code and beautify indent

--- community/telegram/ajax.php
+++ community/telegram/ajax.php
@@ -3,62 +3,65 @@ require('config.php');
 require('links.php');
 
 if (!isset($_GET['recaptcha']) || $_GET['recaptcha'] == '') {
-	fail();
+    fail();
 }
 
 if (isset($_GET['tag']) && array_key_exists($_GET['tag'], $tags)) {
-	$tag = $_GET['tag'];
+    $tag = $_GET['tag'];
 } else {
-	$tag = 'zh-tw';
+    $tag = 'zh-tw';
 }
 
 $curl = curl_init();
 curl_setopt_array($curl, Array(
-	CURLOPT_URL => 'https://www.google.com/recaptcha/api/siteverify',   # Google reCAPTCHA Veritication API
-	CURLOPT_RETURNTRANSFER => True,   # Don't show respone
-	CURLOPT_POST => True,   # Use POST
-	CURLOPT_POSTFIELDS => Array(
-		'secret' => SiteSecret,
-		'response' => $_GET['recaptcha'],
-	),
+    CURLOPT_URL => 'https://www.google.com/recaptcha/api/siteverify',   # Google reCAPTCHA Veritication API
+    CURLOPT_RETURNTRANSFER => True,   # Don't show respone
+    CURLOPT_POST => True,   # Use POST
+    CURLOPT_POSTFIELDS => Array(
+        'secret' => SiteSecret,
+        'response' => $_GET['recaptcha'],
+    ),
 ));
 $data = curl_exec($curl);
 curl_close($curl);
 $data = json_decode($data, True);
 
 if (isset($data['success']) && $data['success']) {   # if verify success
-	success($tag);
+    success($tag);
 } else {
-	fail();
+    fail();
 }
 
-function fail() {
-	$respone = Array(
-		'success' => False
-	);
-	$respone = json_encode($respone);
-	echo $respone;
-	exit;
+function fail()
+{
+    $respone = Array(
+        'success' => False
+    );
+    $respone = json_encode($respone);
+    echo $respone;
+    exit;
 }
 
-function success($tag) {
-	$link = genLinks($tag);
-	$respone = Array(
-		'success' => True,
-		'data' => $link,
-	);
-	$respone = json_encode($respone);
-	echo $respone;
-	exit;
+function success($tag)
+{
+    $link = genLinks($tag);
+    $respone = Array(
+        'success' => True,
+        'data' => $link,
+    );
+    $respone = json_encode($respone);
+    echo $respone;
+    exit;
 }
 
-function genLinks($tag) {
-	global $links;
+function genLinks($tag)
+{
+    global $links;
 
-	foreach ($links as $link) {
-		if (in_array($tag, $link['tags'])) {
-			$result[] = $link;
-		}
-	}
-	return $result;
+    foreach ($links as $link) {
+        if (in_array($tag, $link['tags'])) {
+            $result[] = $link;
+        }
+    }
+    return $result;
 }
