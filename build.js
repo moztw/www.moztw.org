0a24c89717051ae8fd5cb4aedfd7f9d2c242327c
james58899 <james59988@gmail.com>
2018-06-27 15:23:35 +0800

    add comment in build.js

--- build.js
+++ build.js
@@ -4,18 +4,21 @@ const fs = require('fs')
 const path = require('path');
 const mkdirp = require('mkdirp');
 
-const srcDir = './';
-const outDir = 'out/';
-const matcher = '/**/*.shtml';
+const srcDir = './'; // shtml base dir
+const outDir = 'out/'; // build output dir
+const matcher = '/**/*.shtml'; // shtml file matcher
 
 const parser = new ssi(srcDir, outDir, matcher);
 
+// find all shtml file in srcDir
 glob(srcDir + matcher, (err, match) => {
     if (err) throw err;
 
+    // exclude node_module and inc dir
     match.filter(i => !i.match('node_modules') && !i.match('/inc/')).forEach(file => {
             console.log('Reading:', file);
 
+            // skip if parse failed
             try {
                 const out = file.replace(srcDir, outDir).replace('.shtml', '.html');
                 const dir = path.dirname(out);
